7fc4bb4dbf21dd5c16dc1432073fbd6d932f56f1
ntcavichia <new.sophiacavichia@gmail.com>
2026-03-31 14:51:13 -0300

    Atualização do script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const linksPainel = {
     formularios: {
         compras: "https://forms.gle/dfS9AhgTAuEnr7Up8",
@@ -6,6 +7,13 @@ const linksPainel = {
         emprestimo: "https://forms.gle/hRrgjXxd1qBdsX2J8",
         demissao: "https://forms.gle/5ebALfvd5PhGBJM17"
     },
+    advertencias: {
+        suspensao: "https://docs.google.com/document/d/1yO8oIZFzJ_r-t7QFLXJqYShgyk2vHZiNjk1CsYYl1gE/copy?tab=t.0",
+        epi: "https://docs.google.com/document/d/1KM8NvU9YRf9vsHNvmPl4RRDUTIgPNC5uQ3BaKpg0D4I/copy?tab=t.0",
+        falta: "https://docs.google.com/document/d/18VpEreQjqeKetEmdcU_0KndMOjxPMzkhiGlqvimKRdE/copy?tab=t.0",
+        insubordinacao: "https://docs.google.com/document/d/1vwnfJ0brR4peEGZYteVdQWDjbvqLI3pkzlBl-75XDdM/copy?tab=t.0",
+        honra: "https://docs.google.com/document/d/1Y0W90K0mNBocbbi3jUDNIBMKN1DZpyD-CjVSkm-72sU/copy?tab=t.0"
+    },
     contatos: {
         rh: "https://wa.link/ugaorr",
         dp: "https://wa.link/a6v56t"
@@ -13,33 +21,41 @@ const linksPainel = {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
+    
     const acoes = [
+        // Formulários Principais
         { id: "link-compras", url: linksPainel.formularios.compras },
         { id: "link-pessoal", url: linksPainel.formularios.pessoal },
         { id: "link-cargos", url: linksPainel.formularios.cargos },
         { id: "link-emprestimo", url: linksPainel.formularios.emprestimo },
         { id: "link-demissao", url: linksPainel.formularios.demissao },
+        
+        // Botões de Contato
         { id: "btn-rh", url: linksPainel.contatos.rh },
-        { id: "btn-dp", url: linksPainel.contatos.dp },
+        { id: "btn-dp", url: linksPainel.contatos.dp }
     ];
 
     acoes.forEach(acao => {
         const elemento = document.getElementById(acao.id);
         if (elemento) {
             elemento.addEventListener("click", (e) => {
-                e.preventDefault();
+                if (elemento.getAttribute('href') === "#") {
+                    e.preventDefault();
+                }
                 window.open(acao.url, "_blank");
             });
         }
     });
 
-    // Scroll Suave
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({ behavior: 'smooth' });
+            const targetId = this.getAttribute('href');
+            if (targetId !== "#") {
+                e.preventDefault();
+                const target = document.querySelector(targetId);
+                if (target) {
+                    target.scrollIntoView({ behavior: 'smooth' });
+                }
             }
         });
     });
